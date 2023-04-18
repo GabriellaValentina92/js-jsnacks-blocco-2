44895dbc40ad38bbcb63535cd90a10dfe3223680
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-18 18:39:23 +0200

    Add sum odd number

--- snack2.3/js/script.js
+++ snack2.3/js/script.js
@@ -1,2 +1,15 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che 
 // sono in posizione dispari
+
+
+const arr = [1, 2, 3, ,4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for (i = 0; i < arr.length; i++){
+    if(arr[i] % 2 == 1){
+        // sum = sum + arr[i];
+        sum += arr[i] 
+    }
+}
+
+console.log('la somma di tutti gli elementi in posizione dispari è:' + sum);
