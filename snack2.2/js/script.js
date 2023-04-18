fa3d62a646ece6d4277b087035cdf06fb74b6f96
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-18 16:49:41 +0200

    Add math.random operation

--- snack2.2/js/script.js
+++ snack2.2/js/script.js
@@ -3,5 +3,15 @@
 // nomi e una lista di cognomi, 
 // e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
-let Name = [Anna, Marco, Giulio, Antonio, Elena, Piera];
-let Last_name = [Foglietta, Russo, Ranno, Coniglio, Barretta, Giordano];
+let Name = ['Anna', 'Marco', 'Giulio', 'Antonio', 'Elena', 'Piera'];
+let Last_name = ['Foglietta', 'Russo', 'Ranno', 'Coniglio', 'Barretta', 'Giordano'];
+
+let False_Name = Name[Math.floor(Math.random() * Name.lenght)];
+console.log(False_Name)
+let False_lastname = Last_name[Math.floor(Math.random() * Last_name.lenght)];
+console.log(False_lastname)
+
+
+for ( let i = 0; i < Name.lenght, Last_name.lenght; i++){
+    
+}
