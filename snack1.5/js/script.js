9deafac6a18cd1036a419aca88a2639e2ee2c767
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-18 15:32:07 +0200

    Add ex-odd number

---
+++ snack1.5/js/script.js
@@ -0,0 +1,44 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+
+
+// let User = parseInt(prompt('Inserisci un numero'));
+// let User1 = parseInt(prompt('Inserisci un numero'));
+// let User2 = parseInt(prompt('Inserisci un numero'));
+// let User3 = parseInt(prompt('Inserisci un numero'));
+// let User4 = parseInt(prompt('Inserisci un numero'));
+// let User5 = parseInt(prompt('Inserisci un numero'));
+
+// let UserNum = [User, User1, User2, User3, User4, User5];
+// console.log(UserNum)
+
+// let OddNum = [];
+// console.log(OddNum)
+
+// let Odd = false;
+
+// for (let i = 0; i < UserNum.length; i++) {
+//     if (UserNum[i] % 2 == 1){
+//         Odd = true;
+//     }
+// }
+
+// if (Odd){
+// }
+
+let OddNum = [];
+let size = 6;
+
+
+for(let i=0; i<size; i++) {
+
+	OddNum[i] = prompt('inserisci un numero' + (i+1));
+}
+
+if (OddNum[i] % 2 == 1){
+    OddNum.push()
+}
+
+
