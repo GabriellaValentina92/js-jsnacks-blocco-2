1fb9e8c56fab0d4867f547d72dd8f83907edde17
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-04-18 15:54:06 +0200

    Add for adjustment

--- snack1.5/js/script.js
+++ snack1.5/js/script.js
@@ -3,42 +3,16 @@
 // se è dispari inseriscilo nell’array.
 
 
-
-// let User = parseInt(prompt('Inserisci un numero'));
-// let User1 = parseInt(prompt('Inserisci un numero'));
-// let User2 = parseInt(prompt('Inserisci un numero'));
-// let User3 = parseInt(prompt('Inserisci un numero'));
-// let User4 = parseInt(prompt('Inserisci un numero'));
-// let User5 = parseInt(prompt('Inserisci un numero'));
-
-// let UserNum = [User, User1, User2, User3, User4, User5];
-// console.log(UserNum)
-
-// let OddNum = [];
-// console.log(OddNum)
-
-// let Odd = false;
-
-// for (let i = 0; i < UserNum.length; i++) {
-//     if (UserNum[i] % 2 == 1){
-//         Odd = true;
-//     }
-// }
-
-// if (Odd){
-// }
-
 let OddNum = [];
 let size = 6;
 
 
 for(let i=0; i<size; i++) {
+	let Num = parseInt(prompt('inserisci un numero'));
 
-	OddNum[i] = prompt('inserisci un numero' + (i+1));
+    if (Num % 2 == 1){
+        OddNum[i] = Num;
+    }
 }
 
-if (OddNum[i] % 2 == 1){
-    OddNum.push()
-}
-
-
+console.log(OddNum)
--- snack2.2/js/script.js
+++ snack2.2/js/script.js
@@ -2,3 +2,6 @@
 // il Grande Gatsby ha una lista di 
 // nomi e una lista di cognomi, 
 // e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+let Name = [Anna, Marco, Giulio, Antonio, Elena, Piera];
+let Last_name = [Foglietta, Russo, Ranno, Coniglio, Barretta, Giordano];
